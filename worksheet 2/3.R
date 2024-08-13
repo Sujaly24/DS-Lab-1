ae16d70a973a8247042afbb2ee0797b52b9e83a4
Sujaly24 <sujal2903y@gmail.com>
2024-08-13 09:02:33 +0530

    Delete worksheet 2 directory

--- worksheet 2/3.R
+++
@@ -1,7 +0,0 @@
-seat <- read.csv("https://dvats.github.io/assets/course/mth208/seating.csv")
-
-seat$Seat.Number[ which( seat$Roll.No == 241080101)]
-
-myrow <- seat$Roll.No == 241080101
-myseat <- seat[myrow, "Seat.Number"]
-myseat
--- worksheet 2/8.R
+++
@@ -1,6 +0,0 @@
-f <- function(n){
-  return ( (1+1/n) ^n )
-}
-
-plot( x<- c(1:1000), y<- f(c(1:1000)), type='l', xlab="n", ylab="f(n)", main="Euler's Function")
-abline(h=exp(1), col='red')
