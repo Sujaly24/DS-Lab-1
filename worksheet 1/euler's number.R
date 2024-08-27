dc8d3988f9c5bc07804152f6d7500b10e0ea486b
Sujaly24 <sujal2903y@gmail.com>
2024-08-27 09:04:33 +0530

    Delete worksheet 1 directory

--- worksheet 1/euler's number.R
+++
@@ -1,5 +0,0 @@
-euler <- function(n){
-  return ((1+1/n)^n)
-}
-
-euler( as.numeric( readline( prompt = "Enter the number n:")))
--- worksheet 1/factorial.R
+++
@@ -1,8 +0,0 @@
-factorial <- function(n){
-  fact <- 1
-  for (i in 1:n)
-  {fact <- fact*i}
-  return (fact)
-}
-
-factorial( readline (prompt = "enter your number:"))
