4fd4a6460089b151a0713ec3be8ebbd2d23dc3f4
Sujaly24 <sujal2903y@gmail.com>
2024-11-16 08:07:22 +0530

    Delete worksheet 2 directory

--- worksheet 2/4.R
+++
@@ -1,3 +0,0 @@
-setwd("C:/Users/MTH/Desktop/MTH208/worksheet-2-Sujaly24-main")
-
-read.csv("seating.csv")
--- worksheet 2/7.R
+++
@@ -1,1 +0,0 @@
-plot( x<-c(0,2,4,6,8,10), y<-c(0,2,4,6,8,10), type='l', xlab='x', ylab='y', main='Y=X Plot')
